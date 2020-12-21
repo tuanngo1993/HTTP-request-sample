57207324b1c7ad7480f35e8b13c9f6630f13fc2f
Tuan Duc Ngo <tdngo@TUANN-MAC.mgm-edv.de>
2020-12-21 16:04:10 +0700

    refactor by using the third party library called AXIOS

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -5,67 +5,67 @@ const fetchBtn = document
 	.querySelector("button");
 const submitPostBtn = document.getElementById("new-post").querySelector("form");
 
-function sendHTTPRequest(method, url, data) {
-	// // Option 1: Use XMLHttpRequest()
-	// const promise = new Promise((resolve, reject) => {
-	// 	// Create a new XMLHttpRequest object (allow to send HTTPRequest)
-	// const xhr = new XMLHttpRequest();
-	// xhr.setRequestHeader("Content-Type", "application/json");
+// function sendHTTPRequest(method, url, data) {
+// 	// // Option 1*: Use XMLHttpRequest()
+// 	// const promise = new Promise((resolve, reject) => {
+// 	// 	// Create a new XMLHttpRequest object (allow to send HTTPRequest)
+// 	// const xhr = new XMLHttpRequest();
+// 	// xhr.setRequestHeader("Content-Type", "application/json");
 
-	// 	// Configure the request, take 2 args: HTTP method & URL
-	// 	xhr.open(method, url);
+// 	// 	// Configure the request, take 2 args: HTTP method & URL
+// 	// 	xhr.open(method, url);
 
-	// 	// To format JSON data to Array (Option 1)
-	// 	xhr.responseType = "json";
+// 	// 	// Option 1*: To format JSON data to Array
+// 	// 	xhr.responseType = "json";
 
-	// 	// Handle the imcoming response (JSON data)
-	// 	xhr.onload = function () {
-	// 		// cosnt listOfPosts = JSON.parse(xhr.response); // Option 2
-	// 		if (xhr.status >= 200 && xhr.status < 300) {
-	// 			resolve(xhr.response);
-	// 		} else { // Handle non-success status
-	// 			reject(new Error("Something went wrong!!!"));
-	// 		}
-	// 	};
-	// 	// Trigger if a network error, the request fails, does time out, etc.
-	// 	xhr.onerror = function () {
-	// 		reject(new Error("Fail to send a request!!!"));
-	// 	};
+// 	// 	// Handle the imcoming response (JSON data)
+// 	// 	xhr.onload = function () {
+// 	// 		// cosnt listOfPosts = JSON.parse(xhr.response); // Option 2*: Use "parse" method in JSON
+// 	// 		if (xhr.status >= 200 && xhr.status < 300) {
+// 	// 			resolve(xhr.response);
+// 	// 		} else { // Handle non-success status
+// 	// 			reject(new Error("Something went wrong!!!"));
+// 	// 		}
+// 	// 	};
+// 	// 	// Trigger if a network error, the request fails, does time out, etc.
+// 	// 	xhr.onerror = function () {
+// 	// 		reject(new Error("Fail to send a request!!!"));
+// 	// 	};
 
-	// 	// Send the request
-	// 	xhr.send(JSON.stringify(data)); // Convert string to JSON
-	// });
+// 	// 	// Send the request
+// 	// 	xhr.send(JSON.stringify(data)); // Convert string to JSON
+// 	// });
 
-	// return promise;
+// 	// return promise;
 
-	// Option 2: Use fetch() (return a promise), not support for IE
-	return fetch(url, {
-		method: method,
-		body: JSON.stringify(data),
-		headers: {
-			"Content-Type": "application/json", // to tell the server my request has JSON data
-		},
-	})
-		.then((repsonse) => {
-			if (repsonse.status >= 200 && repsonse.status < 300) {
-				return repsonse.json();   // convert to JSON data
-			} else {
-				return repsonse.json().then(errData => {
-					console.log(errData);
-					throw new Error("Something went wrong! - server-side");
-				});
-			}
-		})
-		.catch((err) => {
-			// Trigger if a network error, the request fails, does time out, etc.
-			console.log(err);
-			throw new Error("Something went wrong!");
-		});
-}
+// 	// Option 2: Use fetch() (return a promise), not support for IE
+// 	return fetch(url, {
+// 		method: method,
+// 		body: JSON.stringify(data),
+// 		headers: {
+// 			"Content-Type": "application/json", // to tell the server my request has JSON data
+// 		},
+// 	})
+// 		.then((repsonse) => {
+// 			if (repsonse.status >= 200 && repsonse.status < 300) {
+// 				return repsonse.json();   // convert to JSON data
+// 			} else {
+// 				return repsonse.json().then(errData => {
+// 					console.log(errData);
+// 					throw new Error("Something went wrong! - server-side");
+// 				});
+// 			}
+// 		})
+// 		.catch((err) => {
+// 			// Trigger if a network error, the request fails, does time out, etc.
+// 			console.log(err);
+// 			throw new Error("Something went wrong!");
+// 		});
+// }
 
 async function fetchPost() {
 	listWrapper.innerHTML = "";
-	// Option 1
+	// Option 1**: Use "then"
 	// sendHTTPRequest("GET", "https://jsonplaceholder.typicode.com/posts").then(
 	// 	(responseData) => {
 	// 		const listOfPosts = responseData;
@@ -80,12 +80,16 @@ async function fetchPost() {
 	// );
 
 	try {
-		// Option 2
-		const responseData = await sendHTTPRequest(
-			"GET",
-			"https://jsonplaceholder.typicode.com/posts"
-		);
-		const listOfPosts = responseData;
+		// Option 2**: Use "await"
+		// const responseData = await sendHTTPRequest(
+		// 	"GET",
+		// 	"https://jsonplaceholder.typicode.com/posts"
+		// );
+		// const listOfPosts = responseData;
+		
+		// Option 3: Use "axios"
+		const response = await axios.get("https://jsonplaceholder.typicode.com/posts");
+		const listOfPosts = response.data;
 		for (const post of listOfPosts) {
 			// Create a copy of Node Element
 			const postEl = document.importNode(singlePost.content, true);
@@ -105,12 +109,14 @@ async function createPost(title, content) {
 		body: content,
 		userId: Math.random(),
 	};
-
-	sendHTTPRequest(
-		"POST",
-		"https://jsonplaceholder.typicode.com/posts",
-		newPost
-	);
+	// Option 1 or 2: Use XMLHttpRequest or fetch()
+	// sendHTTPRequest(
+	// 	"POST",
+	// 	"https://jsonplaceholder.typicode.com/posts",
+	// 	newPost
+	// );
+	// Option 3: Use "axios"
+	axios.post("https://jsonplaceholder.typicode.com/posts", newPost);
 }
 
 fetchBtn.addEventListener("click", fetchPost);
@@ -126,9 +132,13 @@ submitPostBtn.addEventListener("submit", (e) => {
 listWrapper.addEventListener("click", (e) => {
 	if (e.target.tagName === "BUTTON") {
 		const postId = e.target.closest("li").id;
-		sendHTTPRequest(
-			"DELETE",
-			`https://jsonplaceholder.typicode.com/posts/${postId}`
-		);
+		// Option 1 or 2: Use XMLHttpRequest or fetch()
+		// sendHTTPRequest(
+		// 	"DELETE",
+		// 	`https://jsonplaceholder.typicode.com/posts/${postId}`
+		// );
+
+		// Option 3: Use "axios"
+		axios.delete(`https://jsonplaceholder.typicode.com/posts/${postId}`);
 	}
 });
